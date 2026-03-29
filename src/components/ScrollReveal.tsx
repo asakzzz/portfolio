8d48ffe8e6269dd9fb65ae29aea765a318d18f15
Leo Girard <leo.girard@epitech.eu>
2026-03-29 18:12:27 +0200

    changing animations for building git pages

--- src/components/ScrollReveal.tsx
+++ src/components/ScrollReveal.tsx
@@ -1,14 +1,14 @@
-'use client';
-import { motion } from 'motion/react';
+// 'use client';
+// import { motion } from 'motion/react';
 
-export default function ScrollReveal({children}) {
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }} 
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, delay:0.05, ease: "easeOut" }}
-    >
-      {children}
-    </motion.div>
-  );
-}
+// export default function ScrollReveal({children}) {
+//   return (
+//     <motion.div
+//       initial={{ opacity: 0, y: 50 }} 
+//       whileInView={{ opacity: 1, y: 0 }}
+//       transition={{ duration: 0.5, delay:0.05, ease: "easeOut" }}
+//     >
+//       {children}
+//     </motion.div>
+//   );
+// }
